416c6515715556e8319a2330bd5f3e426a2e4bdd
vtruonganh <92847275+vtruonganh@users.noreply.github.com>
2023-04-06 09:32:31 +0700

    6/4/2023

--- testnow.php
+++ testnow.php
@@ -12,9 +12,7 @@
 
   <title>Document</title>
 </head>
-
 <body>
-
   <?php
   include('header.php');
   ?>
@@ -29,28 +27,26 @@
           <input type="text" placeholder="Hãy nhập tên của bạn" name="name_user">
           <input name="name_quiz" id="id_quiz" type="text" style="display: none;">
           <p> Chọn bài test mà bạn muốn tham gia</p>
-          <select onchange="onclick1()" id="name_id" name="nametest">
+          <select id="name_id" name="nametest">
 
-            <option style="display: none" >Tên Bài Test</option>
+            <option style="display: none">Tên Bài Test</option>
             <?php
             $myjson = file_get_contents("json/Category.json");
             $json_array = json_decode($myjson, true);
             $a = 0;
-
             foreach ($json_array as $json_arr) { ?>
-              <option name="<?php echo $a++ ?>"  value="<?php echo $json_arr['_id']; ?>"><?php echo $json_arr['name'];  ?></option>
+              <option onchange="onclick1()" name="<?php echo $a++ ?>" value="<?php echo $json_arr['_id']; ?>"><?php echo $json_arr['name']; ?></option>
             <?php
-
             }
             ?>
-
           </select>
           <div style="display: flex;padding-top: 10px;">
             <div style="flex:3"></div>
             <button type="submit">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
                 <path fill="none" d="M0 0h24v24H0z"></path>
-                <path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path>
+                <path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z">
+                </path>
               </svg>
               <a href="/Webinterview2/WebInterview/startest.php">Bắt đầu</a>
             </button>
@@ -58,26 +54,22 @@
           </div>
         </form>
       </div>
-
       <div class="img_main">
         <img src="image/test.png" alt="" />
-
       </div>
     </div>
-
   </section>
   <?php
   include('footer.php')
   ?>
-
 </body>
 <script>
-function onclick1(){
-var name_id =document.getElementById('name_id');
-document.getElementById('id_quiz').value =name_id;
-console.log(name_id.value);
+  function onclick1() {
+    var name_id = document.getElementById('name_id');
+    document.getElementById('id_quiz').value = name_id;
+    console.log(name_id.value);
 
-}
+  }
 </script>
 
 </html>
